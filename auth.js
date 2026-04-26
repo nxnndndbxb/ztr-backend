09c276523d2c473f2e5b98429446a2e19445715b
Wealth zone <htech34641557@gmail.com>
2026-04-26 11:48:25 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-router.post("/login", async (req, res) => {
+router.post("/login", (req, res) => {
   const { wallet } = req.body;
 
-  if (!wallet) return res.status(400).send("Wallet required");
+  if (!wallet) {
+    return res.status(400).send("Wallet required");
+  }
 
   const token = jwt.sign({ wallet }, process.env.JWT_SECRET);
+
   res.json({ token });
 });
 
