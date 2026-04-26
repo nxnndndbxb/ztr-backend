c25c72e301cde36c74345a00726977842b78f683
Wealth zone <htech34641557@gmail.com>
2026-04-26 13:11:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// MAIN TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Backend Working ✅");
+});
+
+// TEST API
+app.get("/api/test", (req, res) => {
+  res.json({ success: true });
 });
 
 module.exports = app;
