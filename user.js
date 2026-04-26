26d19006661331bb134ab4991c044ed1827d3ac5
Wealth zone <htech34641557@gmail.com>
2026-04-26 11:48:55 +0500

    Update user.js

--- user.js
+++ user.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { db } = require("../config/firebase");
 
 router.get("/profile", async (req, res) => {
-  const wallet = req.user.wallet;
+  const wallet = req.headers.wallet;
+
+  if (!wallet) {
+    return res.status(400).send("Wallet missing");
+  }
 
   const snap = await db.ref(`users/${wallet}`).once("value");
+
   res.json(snap.val());
 });
 
